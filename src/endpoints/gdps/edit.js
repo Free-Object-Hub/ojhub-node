3b1bce5f648952c8b50e6658412dd8a0e3e2e754
MIOBOMB <miobomb.real@gmail.com>
2026-06-30 21:19:26 +0700

    gdps ava patch 2

--- src/endpoints/gdps/edit.js
+++ src/endpoints/gdps/edit.js
@@ -101,13 +101,13 @@ export async function edit_(server, url) {
 				let ext = files.img.filename.split('.').pop().toLowerCase();
 				fileNames.img = `${process.env.IMGS}customuser/i${gdpsId}.${ext}`;
 				uploFiles.push(fs.writeFile(fileNames.img, files.img.buffer));
-				fileNames.img = `${HELPER_URL}customuser/i${gdpsId}.${ext}`;
+				fileNames.img = `${HELPER_URL}imgs/customuser/i${gdpsId}.${ext}`;
 			}
 			if (files.ban) {
 				let ext = files.ban.filename.split('.').pop().toLowerCase();
 				fileNames.ban = `${process.env.IMGS}customuser/b${gdpsId}.${ext}`;
 				uploFiles.push(fs.writeFile(fileNames.ban, files.ban.buffer));
-				fileNames.ban = `${HELPER_URL}customuser/b${gdpsId}.${ext}`;
+				fileNames.ban = `${HELPER_URL}imgs/customuser/b${gdpsId}.${ext}`;
 			}
 			uploFiles.push(Gdps.refreshAvatar(
 				gdpsId, 
