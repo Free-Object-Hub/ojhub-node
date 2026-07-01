712a01504cbef5aaa9521d03764a1c1a564a8626
MIOBOMB <miobomb.real@gmail.com>
2026-07-01 21:26:15 +0700

    userpatch

--- src/endpoints/userProfile.js
+++ src/endpoints/userProfile.js
@@ -6,14 +6,14 @@ export async function profile(server, url) {
 		url: url,
 		handler: async (request, reply) => {
 			const uId = parseInt(request.query.id);
-			const cache = await ramDB.g('userF:'+uId);
+			const cache = await ramDB.g('userP:'+uId);
 			if (cache)
 				return cache;
 
 			const user = await Users.fetchById(uId);
 			const json = user.renderPublic();
 
-			await ramDB.s('userF:'+uId, user, 300);
+			await ramDB.s('userP:'+uId, user, 300);
 
 			return json;
 		}
